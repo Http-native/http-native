81244af027b38ca81ca1e785828ac57c334c5dc8
Nadhi-(Kushi) <hello@nadhi.dev>
2026-03-30 16:40:09 +0530

    chore: update npm package

--- opt/runtime.js
+++ opt/runtime.js
@@ -1,5 +1,6 @@
 // keep compatibility with old imports
 // since this is used by benchmarks,
 // its very cheap.
+// will be deprecated soon in the future in favor of direct imports from src/opt/runtime.js
 
 export { createRuntimeOptimizer } from "../src/opt/runtime.js";
